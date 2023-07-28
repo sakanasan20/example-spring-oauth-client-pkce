45e9a0686e7006dd768dacf3cd62580f11d67d2f
Nick <nick.chen@cmp.com.tw>
2023-07-28 08:59:04 +0800

    Initial commit

---
+++ src/main/java/tw/niq/example/ExampleSpringOauthClientPkceApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringOauthClientPkceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringOauthClientPkceApplication.class, args);
+	}
+
+}
